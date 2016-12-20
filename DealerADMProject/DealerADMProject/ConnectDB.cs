279066d91b814efda8b999b5bf03a79aa80b747b
acontrerasrosario <acontrerasrosario@gmail.com>
2016-12-20 12:16:04 -0400

    connectDB String

---
+++ DealerADMProject/DealerADMProject/ConnectDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealerADMProject
+{
+    class ConnectDB
+    {
+
+        string connectDB = "ponga el string connection de su base de datos";
+
+    }
+}
